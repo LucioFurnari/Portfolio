4abee254b01a7297b159508453c1c3b6f8e171ea
LucioFurnari <luciofurnari99@gmail.com>
2023-06-22 22:02:12 -0300

    Create component folder with Nav jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+function Nav() {
+
+  return (
+    <nav className='flex items-center justify-evenly bg-gray-800'>
+      <a href="" className='hover:bg-gray-700 text-[24px] px-[24px] py-[12px] rounded-full'>Home</a>
+      <a href="" className='hover:bg-gray-700 text-[24px] px-[24px] py-[12px] rounded-full'>About</a>
+      <a href="" className='hover:bg-gray-700 text-[24px] px-[24px] py-[12px] rounded-full'>Projects</a>
+      <a href="" className='hover:bg-gray-700 text-[24px] px-[24px] py-[12px] rounded-full'>Contact</a>
+    </nav>
+  )
+}
+
+export default Nav
