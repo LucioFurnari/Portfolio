058858d854a0b7156032ef97777da97128f1831b
LucioFurnari <luciofurnari99@gmail.com>
2023-06-26 18:55:51 -0300

    Import Hero and About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Nav />
+      <Hero />
+      <About />
+    </div>
   )
 }
 
