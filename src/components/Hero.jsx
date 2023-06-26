94a998813e6d35d2f45f90b125752695600c5882
LucioFurnari <luciofurnari99@gmail.com>
2023-06-26 15:54:56 -0300

    Add style to the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 
 function Hero() {
   return (
-    <section>
-      <h2>Hi, I'm Lucio Furnari</h2>
-      <h2>I am a Frontend Developer</h2>
+    <section className={`h-[calc(100vh-60px)] relative before:bg-black/25 before:w-full before:h-full before:absolute bg-hero-pattern bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center`}>
+      <h2 className='text-8xl text-white z-[1]'>Lucio <span className='text-sky-600'>Furnari</span></h2>
+      <h2 className='text-4xl mt-4 text-white z-[1]'>I am a Frontend Developer</h2>
     </section>
   )
 }
+
+export default Hero
