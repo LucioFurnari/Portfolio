921ac94a42b3ab119dbbbe10c2fd3bbc184907fe
LucioFurnari <luciofurnari99@gmail.com>
2023-06-28 17:29:37 -0300

    Add titles for each image of the skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,20 +18,20 @@ function Skills () {
     <section id='skills' className='bg-zinc-100 px-10 py-10 flex flex-col items-center'>
       <h2 className='text-4xl mb-6 font-semibold'>My skills</h2>
       <ul className='grid grid-cols-[minmax(100px,_200px)_minmax(100px,_200px)_minmax(100px,_200px)_minmax(100px,_200px)_minmax(100px,_200px)] justify-items-center gap-8'>
-            <Card url={reactIcon} altText='React js icon' />
-            <Card url={tailwindIcon} altText='Tailwind css icon'/>
-            <Card url={bootstrapIcon} altText='Bootstrap icon' />
-            <Card url={sassIcon} altText='Sass icon' />
-            <Card url={htmlIcon} altText='Html icon' />
-            <Card url={cssIcon} altText='Css icon' />
-            <Card url={javascriptIcon} altText='Javascript icon' />
-            <Card url={nodejsIcon} altText='Node js icon' />
-            <Card url={firebaseIcon} altText='Firebase icon' />
-            <Card url={typescriptIcon} altText='Typescript icon' />
-            <Card url={jestIcon} altText='Jest icon' />
-            <Card url={webpackIcon} altText='Webpack icon' />
-            <Card url={viteIcon} altText='Vite icon' />
-            <Card url={gitIcon} altText='Git icon'/>
+            <Card url={reactIcon} altText='React js icon' imgText='React Js' />
+            <Card url={tailwindIcon} altText='Tailwind css icon' imgText='Tailwind Css' />
+            <Card url={bootstrapIcon} altText='Bootstrap icon' imgText='Bootstrap' />
+            <Card url={sassIcon} altText='Sass icon' imgText='Sass' />
+            <Card url={htmlIcon} altText='Html icon' imgText='Html' />
+            <Card url={cssIcon} altText='Css icon' imgText='Css' />
+            <Card url={javascriptIcon} altText='Javascript icon'  imgText='Javascript' />
+            <Card url={nodejsIcon} altText='Node js icon' imgText='Node Js' />
+            <Card url={firebaseIcon} altText='Firebase icon' imgText='Firebase' />
+            <Card url={typescriptIcon} altText='Typescript icon' imgText='Typescript' />
+            <Card url={jestIcon} altText='Jest icon' imgText='Jest' />
+            <Card url={webpackIcon} altText='Webpack icon' imgText='Webpack' />
+            <Card url={viteIcon} altText='Vite icon' imgText='Vite' />
+            <Card url={gitIcon} altText='Git icon' imgText='Git' />
       </ul>
     </section>
   )
@@ -41,11 +41,11 @@ export default Skills
 
 function Card (props) {
   // eslint-disable-next-line react/prop-types
-  const { url, altText } = props
+  const { url, altText, imgText } = props
   return (
-    <li className='bg-white p-4 flex items-center w-full rounded-lg border-2 border-zinc-500'>
+    <li className='bg-white p-4 flex flex-col items-center w-full rounded-lg border-2 border-zinc-500'>
       <img src={url} alt={altText} className='w-full h-auto'></img>
-      <p></p>
+      <p className='mt-4 text-lg'>{imgText}</p>
     </li>
   )
 }
