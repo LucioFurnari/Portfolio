5046c8acd0ade491c7988206281fb83541a0bfce
LucioFurnari <luciofurnari99@gmail.com>
2023-06-28 16:53:31 -0300

    Import skills, and add relative class to the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import About from './components/About'
+import Skills from './components/Skills'
 
 function App() {
 
   return (
-    <div>
+    <div className='relative'>
       <Nav />
       <Hero />
       <About />
+      <Skills />
     </div>
   )
 }
