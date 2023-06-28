4f01a942aecf92db05ced04e937d06382d59c9e7
LucioFurnari <luciofurnari99@gmail.com>
2023-06-28 16:52:31 -0300

    Change nav to be fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,10 @@
 function Nav() {
 
   return (
-    <nav className='flex items-center justify-evenly bg-stone-900'>
+    <nav className='flex items-center justify-evenly bg-stone-900 fixed top-0 right-0 w-full z-20'>
       <a href="#home" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Home</a>
       <a href="#about" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>About</a>
+      <a href="#skills" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Skills</a>
       <a href="#projects" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Projects</a>
       <a href="#contact" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Contact</a>
     </nav>
