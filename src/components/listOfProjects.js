1bd66776d3eb496d609c99f92b74cee38d5f2e04
LucioFurnari <luciofurnari99@gmail.com>
2023-06-29 18:40:46 -0300

    Create a js file with a array of projects

---
+++ src/components/listOfProjects.js
@@ -0,0 +1,40 @@
+import reactIcon from '../assets/react_icon.svg';
+import tailwindIcon from '../assets/tailwind_icon.svg';
+import firebaseIcon from '../assets/firebase_icon.svg';
+import reactRouterIcon from '../assets/react-router_icon.svg';
+import viteIcon from '../assets/vite_icon.svg';
+
+
+export const onlineShopProject ={
+  projectTitle: 'Online shop', 
+  projectDescription: 'A full stack single page application built with React, Tailwind css and Firebase for back end, that allow user to register, login, create a shop cart and buy products.', 
+  arrayOfTech: [ 
+    {
+      url: reactIcon,
+      techName: 'React Js',
+      id: 1
+    },
+    {
+      url: tailwindIcon,
+      techName: 'Tailwind Css',
+      id: 2
+    },
+    {
+      url: firebaseIcon,
+      techName: 'Firebase',
+      id: 3
+    },
+    {
+      url: reactRouterIcon,
+      techName: 'React router',
+      id: 4
+    },
+    {
+      url: viteIcon,
+      techName: 'Vite',
+      id: 5
+    }
+  ], 
+  liveLink: 'https://luciofurnari.github.io/shopping-cart/', 
+  codeLink: 'https://github.com/LucioFurnari/shopping-cart',
+}
