738c43bafa78097965347e08381ef5e0cddafb15
LucioFurnari <luciofurnari99@gmail.com>
2023-06-30 20:14:05 -0300

    Remove white space in class

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,11 @@ function Nav() {
 
   return (
     <nav className='flex items-center justify-evenly bg-stone-900 fixed top-0 right-0 w-full z-20'>
-      <a href="#home" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Home</a>
-      <a href="#about" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>About</a>
-      <a href="#skills" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Skills</a>
-      <a href="#projects" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Projects</a>
-      <a href="#contact" className=' text-slate-100 text-[24px] px-[24px] py-[12px] rounded-full'>Contact</a>
+      <a href="#home" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Home</a>
+      <a href="#about" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>About</a>
+      <a href="#skills" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Skills</a>
+      <a href="#projects" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Projects</a>
+      <a href="#contact" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Contact</a>
     </nav>
   )
 }
