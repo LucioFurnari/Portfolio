16eddb24f9a7a2b4ebf45c3ab607798d07ef5960
LucioFurnari <luciofurnari99@gmail.com>
2023-06-30 20:13:40 -0300

    Improve style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ function Contact () {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_ym7he4i', 'template_h7ru42j', form.current, 'XA_Y_tNCYMI7ouZHp')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -16,15 +16,21 @@ function Contact () {
   };
 
   return (
-    <section id="contact">
-        <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+    <section id='contact' className='flex justify-center bg-zinc-100 py-12'>
+      <form ref={form} onSubmit={sendEmail} className='flex flex-col items-center'>
+        <fieldset className='relative w-96 mb-4'>
+          <input id='user-name' className='w-full border-b-[1px] border-black p-4 peer' type='text' name='user_name' />
+          <label htmlFor='user-name' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm' >Name</label>
+        </fieldset>
+        <fieldset className='relative w-96 mb-4'>
+          <input id='user-email' className='w-full border-b-[1px] border-black p-4 peer' type="email" name="user_email" />
+          <label htmlFor='user-email' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm'>Email</label>
+        </fieldset>
+        <fieldset className='relative w-96 mb-4'>
+          <textarea id='user-message' className='w-full border-b-[1px] border-black p-4 peer' name="message" />
+          <label htmlFor='user-message' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm'>Message</label>
+        </fieldset>
+        <input type='submit' value='Send' className='bg-red-600/90 hover:bg-red-400 text-white transition-colors px-14 py-2 rounded-full text-lg cursor-pointer' />
       </form>
     </section>
   )
