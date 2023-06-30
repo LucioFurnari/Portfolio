7ad9f935d260436cadbc4fe5e486f515b9f21445
LucioFurnari <luciofurnari99@gmail.com>
2023-06-30 20:14:26 -0300

    Center text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@
 function Hero() {
   return (
     <section id='home' className={`min-h-screen relative before:bg-black/25 before:w-full before:h-full before:absolute bg-hero-pattern bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center`}>
-      <h2 className='text-8xl text-white z-[1]'>Lucio <span className='text-red-600'>Furnari</span></h2>
-      <h2 className='text-4xl mt-4 text-white z-[1]'>I am a Frontend Developer</h2>
+      <h2 className='text-8xl text-white z-[1] text-center'>Lucio <span className='text-red-600'>Furnari</span></h2>
+      <h2 className='text-4xl mt-4 text-white z-[1] text-center'>I am a Frontend Developer</h2>
     </section>
   )
 }
