62a2f5711efee4832bc3cc1a68871365aa92e7b6
LucioFurnari <luciofurnari99@gmail.com>
2023-07-01 18:09:13 -0300

    Import and add new icon for the nav menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,18 @@
+import homeIcon from '../assets/home_icon.svg';
+import aboutIcon from '../assets/about_icon.svg';
+import skillsIcon from '../assets/skills_icon.svg';
+import projectIcon from '../assets/projects_icon.svg';
+import contactIcon from '../assets/contact_icon.svg';
 
 function Nav() {
 
   return (
-    <nav className='flex items-center justify-evenly bg-stone-900 fixed top-0 right-0 w-full z-20'>
-      <a href="#home" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Home</a>
-      <a href="#about" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>About</a>
-      <a href="#skills" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Skills</a>
-      <a href="#projects" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Projects</a>
-      <a href="#contact" className='text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'>Contact</a>
+    <nav className='flex flex-col items-center justify-evenly bg-stone-900 fixed top-0 left-0 h-full z-20 p-4'>
+      <a href="#home" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={homeIcon}></img> Home</a>
+      <a href="#about" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={aboutIcon}></img> About</a>
+      <a href="#skills" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={skillsIcon}></img> Skills</a>
+      <a href="#projects" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={projectIcon}></img> Projects</a>
+      <a href="#contact" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={contactIcon}></img> Contact</a>
     </nav>
   )
 }
