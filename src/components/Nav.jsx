a8bbe5e3f8b351d4a16c75752b4b50396c36a3b7
LucioFurnari <luciofurnari99@gmail.com>
2023-07-01 19:41:37 -0300

    Make the nav bar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,12 +7,12 @@ import contactIcon from '../assets/contact_icon.svg';
 function Nav() {
 
   return (
-    <nav className='flex flex-col items-center justify-evenly bg-stone-900 fixed top-0 left-0 h-full z-20 p-4'>
-      <a href="#home" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={homeIcon}></img> Home</a>
-      <a href="#about" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={aboutIcon}></img> About</a>
-      <a href="#skills" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={skillsIcon}></img> Skills</a>
-      <a href="#projects" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={projectIcon}></img> Projects</a>
-      <a href="#contact" className='flex items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-8 mr-4' src={contactIcon}></img> Contact</a>
+    <nav className='overflow-x-scroll flex sm:flex-col items-center justify-evenly bg-stone-900 fixed bottom-0 sm:top-0 left-0 w-full sm:w-auto sm:h-full z-20 p-4'>
+      <a href="#home" className='flex flex-col xl:flex-row items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-4 sm:w-6 mb-1 xl:mb-0 xl:w-8 xl:mr-4' src={homeIcon}></img><span className='text-xs xl:text-xl'>Home</span></a>
+      <a href="#about" className='flex flex-col xl:flex-row items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-4 sm:w-6 mb-1 xl:mb-0 xl:w-8xl:mr-4' src={aboutIcon}></img><span className='text-sm xl:text-xl'>About</span></a>
+      <a href="#skills" className='flex flex-col xl:flex-row items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-4 sm:w-6 mb-1 xl:mb-0 xl:w-8 xl:mr-4' src={skillsIcon}></img><span className='text-sm xl:text-xl'>Skills</span></a>
+      <a href="#projects" className='flex flex-col xl:flex-row items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-4 sm:w-6 mb-1 xl:mb-0 xl:w-8 xl:mr-4' src={projectIcon}></img><span className='text-sm xl:text-xl'>Projects</span></a>
+      <a href="#contact" className='flex flex-col xl:flex-row items-center w-full text-slate-100 hover:bg-red-500 transition-colors text-[24px] px-[24px] py-[12px] rounded-full'><img className='w-4 sm:w-6 mb-1 xl:mb-0 xl:w-8 xl:mr-4' src={contactIcon}></img><span className='text-sm xl:text-xl'>Contact</span></a>
     </nav>
   )
 }
