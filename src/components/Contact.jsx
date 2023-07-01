b4d691b2e5c0f914c9d845ee282f4814e255d5a1
LucioFurnari <luciofurnari99@gmail.com>
2023-07-01 01:53:24 -0300

    Fix input label

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,16 +19,16 @@ function Contact () {
     <section id='contact' className='flex justify-center bg-zinc-100 py-12'>
       <form ref={form} onSubmit={sendEmail} className='flex flex-col items-center'>
         <fieldset className='relative w-96 mb-4'>
-          <input id='user-name' className='w-full border-b-[1px] border-black p-4 peer' type='text' name='user_name' />
-          <label htmlFor='user-name' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm' >Name</label>
+          <input id='user-name' className='outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' type='text' name='user_name' placeholder=" " />
+          <label htmlFor='user-name' className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base' >Name</label>
         </fieldset>
         <fieldset className='relative w-96 mb-4'>
-          <input id='user-email' className='w-full border-b-[1px] border-black p-4 peer' type="email" name="user_email" />
-          <label htmlFor='user-email' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm'>Email</label>
+          <input id='user-email' className='outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' type="email" name="user_email"  placeholder=" "/>
+          <label htmlFor='user-email' className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base'>Email</label>
         </fieldset>
         <fieldset className='relative w-96 mb-4'>
-          <textarea id='user-message' className='w-full border-b-[1px] border-black p-4 peer' name="message" />
-          <label htmlFor='user-message' className='absolute translate-y-4 left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm'>Message</label>
+          <textarea id='user-message' className='outline-none w-full border-b-[1px] border-black focus:border-b-2 transition-all p-4 peer' name="message" placeholder=" "/>
+          <label htmlFor='user-message' className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base'>Message</label>
         </fieldset>
         <input type='submit' value='Send' className='bg-red-600/90 hover:bg-red-400 text-white transition-colors px-14 py-2 rounded-full text-lg cursor-pointer' />
       </form>
