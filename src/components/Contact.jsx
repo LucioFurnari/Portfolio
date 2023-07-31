f0e16168359eb54402a6ba416a86a8386047c808
LucioFurnari <luciofurnari99@gmail.com>
2023-07-30 22:02:22 -0300

    Add image for the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import emailjs from '@emailjs/browser';
+import ContactImage from '../assets/undraw_mailboxs.svg'
 
 function Contact () {
   const form = useRef();
@@ -18,7 +19,8 @@ function Contact () {
   return (
     <section id='contact' className='bg-zinc-100 pb-40 pt-20 sm:py-20 px-4 sm:px-40 sm:ml-28'>
       <h2 className='z-[1] text-4xl relative font-semibold mb-6 pb-4 after:content-[" "] after:absolute after:bottom-0 after:left-0 after:bg-red-600 after:w-24 after:h-2 '>Get in touch</h2>
-      <form ref={form} onSubmit={sendEmail}>
+      <div className="flex items-center">
+      <form className="w-full" ref={form} onSubmit={sendEmail}>
         <fieldset className='relative max-w-[24rem] mb-4 '>
           <input id='user-name' className='outline-none w-full border-b-[1px] border-black p-4 focus:border-b-2 transition-all peer' type='text' name='user_name' placeholder=" " />
           <label htmlFor='user-name' className='absolute text-sm left-4 transition-transform peer-focus:-translate-y-0 peer-focus:text-sm peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base' >Name</label>
@@ -33,6 +35,8 @@ function Contact () {
         </fieldset>
         <input type='submit' value='Send' className='bg-red-600/90 hover:bg-red-400 text-white transition-colors px-14 py-2 rounded-full text-lg cursor-pointer' />
       </form>
+      <img className="w-full h-auto px-10 lg:block hidden" src={ContactImage} alt="Mailbox image"></img>
+      </div>
     </section>
   )
 }
